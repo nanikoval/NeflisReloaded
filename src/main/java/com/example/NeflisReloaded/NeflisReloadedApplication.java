dfeab75f6064dc09a1d177aeb88f7d9ac11fb066
nanikoval <nataliakoval@live.com.ar>
2019-09-25 20:44:34 -0300

    Creamos proyecto NeflisReloaded

---
+++ src/main/java/com/example/NeflisReloaded/NeflisReloadedApplication.java
@@ -0,0 +1,13 @@
+package com.example.NeflisReloaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeflisReloadedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeflisReloadedApplication.class, args);
+	}
+
+}
